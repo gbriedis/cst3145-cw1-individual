c8a2a416ac25568cf06572eb90a84091e8a03d45
Ginters <GB664@live.mdx.ac.uk>
2021-11-19 13:24:41 +0000

    moved lessons to lessons.js

---
+++ assets/js/lessons.js
@@ -0,0 +1,12 @@
+let lessonsArray = [
+    {id: 1, subject: 'Maths', location: 'London', price: 100, spaces: 5, image: "assets/img/math-icon.png"},
+    {id: 2, subject: 'English', location: 'London', price: 80, spaces: 5, image: "assets/img/english-icon.png"},
+    {id: 3, subject: 'Science', location: 'London', price: 75, spaces: 5, image: "assets/img/science-icon.png"},
+    {id: 4, subject: 'Sport', location: 'London', price: 20, spaces: 5 , image: "assets/img/sport-icon.png"},
+    {id: 5, subject: 'Computer Science', location: 'London', price: 125, spaces: 5, image: "assets/img/computer-icon.png"},
+    {id: 6, subject: 'Maths', location: 'Peterborough', price: 25, spaces: 5, image: "assets/img/math-icon.png"},
+    {id: 7, subject: 'English', location: 'Peterborough', price: 20, spaces: 5, image: "assets/img/english-icon.png"},
+    {id: 8, subject: 'Science', location: 'Peterborough', price: 15, spaces: 5, image: "assets/img/science-icon.png"},
+    {id: 9, subject: 'Sport', location: 'Peterborough', price: 5, spaces: 5, image: "assets/img/sport-icon.png"},
+    {id: 10, subject: 'Computer Science', location: 'Peterborough', price: 50, spaces: 5, image: "assets/img/computer-icon.png"}
+]
